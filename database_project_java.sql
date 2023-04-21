7bf04daac5f5ed63b96c5fc68d672e0bfcf62714
Rishabh Deo Singh <99266473+Rishabh-mikku@users.noreply.github.com>
2023-04-21 23:44:19 +0530

    Java Project (Voice Assistant)

--- database_project_java.sql
+++
@@ -1,5 +0,0 @@
-create database users;
-use users;
-
-create table registration(Name varchar(50), RegNo int, Mobile int, Username varchar(50), Password varchar(100), PRIMARY KEY (Username));
-desc registration;
